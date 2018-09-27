a12097500d1a3c717d53ce7b52deca619d570d68
DHgit1 <33430196+DHgit1@users.noreply.github.com>
2018-09-26 23:35:31 -0700

    updates deploy to heroku

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.static('./dist/ng6-proj'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/your_app_name/index.html'));
+
+res.sendFile(path.join(__dirname,'/dist/ng6-proj/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
